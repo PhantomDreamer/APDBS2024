f7c63dece4a60838c08fa75e8585c06747b70030
s20743 <s20743@pjwstk.edu.pl>
2024-03-25 05:19:54 +0900

    Feature implementation

--- Tut1/Task2/ConsoleApp1/Program.cs
+++ Tut1/Task2/ConsoleApp1/Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mod1");
 Console.WriteLine("Mod2");
 Console.WriteLine("Mod3");
+
+int[] arr = { 1, 6, 7, 8, 2, 3, 5, 7, 2, 8 };
+
+static void avgNumbers(int[] calc) {
+    double sum = 0;
+    for (int i = 0; i < calc.Length; i++) {
+        sum += calc[i];
+    }
+    Console.WriteLine(sum / calc.Length);
+}
+
+avgNumbers(arr);
