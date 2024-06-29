87cbc6cbe44da0444d133e8a2c08ebbf6e93b163
s20743 <s20743@pjwstk.edu.pl>
2024-06-29 23:42:35 +0900

    Task 5 solution

---
+++ Task5/Task5/Controllers/AnimalsController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Task5.Models;
+
+namespace Task5.Controllers {
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase {
+        [HttpGet]
+        public IActionResult GetAnimals([FromQuery] string orderBy = "name") {
+            var validParams = new List<string> { "name", "description", "category", "area" };
+
+            if (!validParams.Contains(orderBy.ToLower())) {
+                return BadRequest("Invalid parameter");
+            }
+
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+
+            using var connection = new SqlConnection("Data Source=db-mssql16.pjwstk.edu.pl;Integrated Security=SSPI;");
+            connection.Open();
+
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT IdAnimal, Name, Description, Category, Area FROM Animals ORDER BY @orderBy ASC";
+            cmd.Parameters.AddWithValue("@orderBy", orderBy);
+
+            var dr = cmd.ExecuteReader();
+            var animals = new List<Animal>();
+
+            while (dr.Read()) {
+                var animal = new Animal {
+                    IdAnimal = (int)dr["IdAnimal"],
+                    Name = dr.IsDBNull(1) ? null : dr["Name"].ToString(),
+                    Description = dr["Description"].ToString(),
+                    Category = dr.IsDBNull(3) ? null : dr["Category"].ToString(),
+                    Area = dr.IsDBNull(4) ? null : dr["Area"].ToString()
+                };
+                animals.Add(animal);
+            }
+            cmd.Dispose();
+            connection.Dispose();
+
+            return Ok(animals);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAnimal(Animal animal) {
+            using var connection = new SqlConnection("ConnectionStrings:DefaultConnection");
+            connection.Open();
+
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "INSERT INTO Animal(Name, Description, Category, Area) VALUES(@Name, @Description, @Category, @Area)";
+            cmd.Parameters.AddWithValue("@Name", animal.Name);
+            cmd.Parameters.AddWithValue("@Description", animal.Description);
+            cmd.Parameters.AddWithValue("@Category", animal.Category);
+            cmd.Parameters.AddWithValue("@Area", animal.Area);
+
+            cmd.Dispose();
+            connection.Dispose();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{idAnimal:int}")]
+        public IActionResult UpdateAnimal(int idAnimal, Animal animal) {
+            if(idAnimal != animal.IdAnimal) {
+                return BadRequest("The ID of the specified animal and the data do not match");
+            }
+            using var connection = new SqlConnection("ConnectionStrings:DefaultConnection");
+            connection.Open();
+
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "UPDATE Animal SET Name=@Name, Description=@Description, Category=@Category, Area=@Area WHERE IdAnimal = @IdAnimal";
+            cmd.Parameters.AddWithValue("@IdAnimal", animal.IdAnimal);
+            cmd.Parameters.AddWithValue("@Name", animal.Name);
+            cmd.Parameters.AddWithValue("@Description", animal.Description);
+            cmd.Parameters.AddWithValue("@Category", animal.Category);
+            cmd.Parameters.AddWithValue("@Area", animal.Area);
+
+            cmd.Dispose();
+            connection.Dispose();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{idAnimal:int}")]
+        public IActionResult DeletePet(int idAnimal) {
+            using var connection = new SqlConnection("ConnectionStrings:DefaultConnection");
+            connection.Open();
+
+            using var cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "DELETE FROM Animal WHERE IdAnimal = @IdAnimal";
+            cmd.Parameters.AddWithValue("@IdAnimal", idAnimal);
+
+            cmd.Dispose();
+            connection.Dispose();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Task5/Task5/Models/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task5.Models {
+    public class Animal {
+        public int IdAnimal { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Category { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Area { get; set; }
+    }
+}
